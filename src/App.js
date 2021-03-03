6c33ba52f8248244dcbf8bed32c89598110ce857
mrsatyam <mrsatyamjoshi@gmail.com>
2021-03-03 20:22:40 +0530

    using states in class based component and handle events.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,38 @@ import PersonFunction from './Person/PersonFunction'
 
 
 class App extends Component {
+  state = {
+    persons:[
+        {name: 'Satyam', age:24},
+        {name: 'Manisha', age:54},
+        {name: 'Kabbu', age:5},
+        {name: 'Nishu', age:104} 
+    ]
+}
+
+switchNameHandler = ()=>{
+ this.setState({ persons:[
+  {name: 'Satyam', age:24},
+  {name: 'Manisha', age:14},
+  {name: 'Kabbu', age:50},
+  {name: 'Nishu', age:10} 
+]})
+}
  render(){
    return(
    <div className="App" >
-     This is my first react app.
-     <PersonClass name="Satyam Joshi" age="24"> My hobby: code</PersonClass>
+     {/* <PersonClass name="Satyam Joshi" age="24"> My hobby: code</PersonClass>
      <PersonClass  name="Muskan Garg" age="25"> My hobby: eating</PersonClass>
      <PersonClass  name="Nidhi Garg" age="27"> My hobby : dancing</PersonClass>
-     <PersonFunction name = "Fun" height = "10"> My job: nada</PersonFunction>
+     <PersonFunction name = "Fun" height = "10"> My job: nada</PersonFunction> */}
+     
+     <PersonClass name={this.state.persons[0].name} age={this.state.persons[0].age}> My hobby: code</PersonClass>
+     <PersonClass  name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobby: eating</PersonClass>
+     <PersonClass  name={this.state.persons[2].name} age={this.state.persons[2].age}> My hobby: dancing</PersonClass>
+     <PersonFunction  name={this.state.persons[3].name} age={this.state.persons[3].age}> My job: nada</PersonFunction> 
+     <br></br>
+     <button style={{color:'Blue',backgroundColor:'Red'}} onClick = {this.switchNameHandler}> Switch </button>
+     
    </div>
    )
    //This is what the above jsx is compiled to: 
--- src/Person/PersonFunction.js
+++ src/Person/PersonFunction.js
@@ -3,9 +3,8 @@ import React from 'react'
 const PersonFunction = (props)=>{
   return <div> 
     <h2>function component</h2> 
-    <h3>Hi, I'm {props.name} and I'm {Math.floor(Math.random()*props.height+10)} ft. tall</h3>
-    {props.children}
-
+     <h3>Hi, I'm {props.name} and I'm {props.age} years old.</h3>
+    {props.children} 
   </div>
 
 
